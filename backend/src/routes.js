eb3ab7935b8675e15c3ef02a04114cb9d38f1e39
Philipe A. Marques <lipe.aguiar.marques@gmail.com>
2020-04-17 13:29:59 -0300

    Feito loader de models

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,8 +1,14 @@
 const { Router } = require('express');
 const routes = new Router();
+const User = require('./app/models/User');
 
-routes.get('/', (req, res) => {
-  return res.json({message: 'Hello World'});
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Philipe',
+    email: 'lipe.aguiar@gmail.com',
+    password_hash: '12308140',
+  });
+  return res.json(user);
 });
 
 module.exports = routes;

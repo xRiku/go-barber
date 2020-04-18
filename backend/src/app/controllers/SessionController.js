a3fb26865ce809076b51013cbe7d153e99327ac4
Philipe A. Marques <lipe.aguiar.marques@gmail.com>
2020-04-18 04:03:01 -0300

    login feito token jwt gerado variavel para gerar token adicionada a .env feito arquivo de autorização na pasta config que contem secret e expires in

--- backend/src/app/controllers/SessionController.js
+++ backend/src/app/controllers/SessionController.js
@@ -1,15 +1,31 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
+const authConfig = require('../../config/auth');
+
 
 class SessionController {
   async store(request, response) {
     const { email, password } = request.body;
-    const user = User.findOne({ where: { email } });
+    const user = await User.findOne({ where: { email } });
     if (!user) {
       return response.status(401).json({ error: "User not found" });
     }
-    if (!await user.checkPassword(password)) {
+    if (!(await user.checkPassword(password))) {
       return response.status(401).json({ error: "Password does not match" })
     }
+
+    const { id, name } = user;
+
+    return response.json({
+      user: {
+        id,
+        name,
+        email,
+      },
+      token: jwt.sign({ id }, authConfig.secret, {
+        expiresIn: authConfig.expiresIn,
+      }),
+    });
   }
 }
 
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,7 +1,9 @@
 const { Router } = require('express');
 const routes = new Router();
 const UserController = require('./app/controllers/UserController');
+const SessionController = require('./app/controllers/SessionController');
 
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
 
 module.exports = routes;

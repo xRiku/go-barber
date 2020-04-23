33aadf6bbfbf577202c691aecda040a6287efa5e
Philipe A. Marques <lipe.aguiar.marques@gmail.com>
2020-04-22 22:23:52 -0300

    upload de files funcional multer adicionado as dependencias

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,13 @@
 const { Router } = require('express');
-const routes = new Router();
+const multer = require('multer');
+const multerConfig = require('./config/multer');
+
 const UserController = require('./app/controllers/UserController');
 const SessionController = require('./app/controllers/SessionController');
 const authMiddleware = require('./app/middlewares/auth');
 
+const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
@@ -11,5 +15,9 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.post('/files', upload.single('file'), (req, res) => {
+  return res.json({ ok: true });
+});
 
 module.exports = routes;

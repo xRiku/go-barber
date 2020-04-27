0d8a9484e92a36ac1ea647cd6aab1cb42ac8f595
Philipe A. Marques <lipe.aguiar.marques@gmail.com>
2020-04-26 23:44:34 -0300

    Feita listagem de notificações de um provider

--- backend/src/app/controllers/NotificationController.js
+++ backend/src/app/controllers/NotificationController.js
@@ -1,9 +1,26 @@
 const Notification = require('../models/Notification');
+const User = require('../models/User');
 
 class NotificationController {
   async index(req, res) {
-    return res.json();
-  };  
+    const isProvider = await User.findOne({
+      where: {
+        id: req.userId,
+        provider: true,
+      }
+    });
+
+    if (!isProvider) {
+      return res
+        .status(401)
+        .json({ error: 'Only providers can load notifications' });
+    }
+
+    const notifications = await Notification.findAll({ where: { user: req.userId } });
+
+
+    return res.json(notifications);
+  };
 }
 
 module.exports = new NotificationController();

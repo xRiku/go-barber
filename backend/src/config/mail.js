54e0d61b71d45405cd56da3b74cb79ad076feeb6
Philipe A. Marques <lipe.aguiar.marques@gmail.com>
2020-04-30 23:06:01 -0300

    Nodemailer adicionado a dependencias criado arquivo mail.js para configuração do nodemailer

---
+++ backend/src/config/mail.js
@@ -0,0 +1,12 @@
+module.exports = {
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  secure: false,
+  auth: {
+    user: "e3f4b87fe8d9c8",
+    pass: "f7729986aeaaa3"
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>'
+  }
+}

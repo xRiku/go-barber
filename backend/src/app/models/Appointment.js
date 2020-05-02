c7f7761a9982b0e9c8606d4baf888276d848b9c1
Philipe A. Marques <lipe.aguiar.marques@gmail.com>
2020-05-02 15:27:25 -0300

    adicionados campos virtuais de cancelavel e se a data ja passou no model de agendamentos

--- backend/src/app/models/Appointment.js
+++ backend/src/app/models/Appointment.js
@@ -1,4 +1,6 @@
 const { Sequelize, Model } = require('sequelize');
+const { isBefore, subHours } = require('date-fns');
+
 
 class Appointment extends Model {
   static init(sequelize) {
@@ -6,6 +8,18 @@ class Appointment extends Model {
       {
         date: Sequelize.DATE,
         canceled_at: Sequelize.DATE,
+        past: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return isBefore(this.date, new Date());
+          }
+        },
+        cancelable: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return isBefore(new Date(), subHours(this.date, 2));
+          }
+        }
       },
       {
         sequelize,
